95ebeacf5d757fe400cd4ca44f599e8a7e3fee0d
tapan77777 <72134688+tapan77777@users.noreply.github.com>
2024-07-19 14:55:59 +0530

    data added

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const app = express()
 
 const port = 1000
 
+const githubData = {
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 90,
+    "public_gists": 4,
+    "followers": 26326,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-06-22T18:02:17Z"
+    }
+
+
 app.get('/', (req, res) => {
     res.send('Hello tapan')
 })
@@ -14,6 +50,10 @@ app.get('/youtube', (req, res ) => {
 
 app.get('/login', (req, res) => {
     res.send("<h1>login</h1>")
+})
+
+app.get('/data', (req,res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
